d8494c2bad3c1f09dc20a9e4c3dd3f8b1bdecb21
Zafar Ali <zafarale@gmail.com>
2025-04-12 17:30:17 +0100

    Rust Project setup

---
+++ target/debug/deps/zest-2a32c09c46a642ea.d
@@ -0,0 +1,5 @@
+/Volumes/dev/ws/learn_programming/target/debug/deps/libzest-2a32c09c46a642ea.rmeta: src/main.rs
+
+/Volumes/dev/ws/learn_programming/target/debug/deps/zest-2a32c09c46a642ea.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/zest-aeddb2bd9493421d.d
@@ -0,0 +1,5 @@
+/Volumes/dev/ws/learn_programming/target/debug/deps/libzest-aeddb2bd9493421d.rmeta: src/main.rs
+
+/Volumes/dev/ws/learn_programming/target/debug/deps/zest-aeddb2bd9493421d.d: src/main.rs
+
+src/main.rs:
