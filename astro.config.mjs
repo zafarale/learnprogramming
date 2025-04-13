d843ba72e688d9e6aba28c2463d7388805a59726
Zafar Ali <zafarale@gmail.com>
2025-04-13 14:47:57 +0100

    Updated configuration for github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://zafarale.github.io',  // Replace with your GitHub username
+  base: '/learnprogramming',  // Replace with your repository name
   vite: {
     plugins: [tailwindcss()],
   },
