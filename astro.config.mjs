08672e361dcb109a2c169dfb4db98616699ad76d
Zafar Ali <zafarale@gmail.com>
2025-04-14 07:51:03 +0100

    added astro icons/hugeicon

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://zafarale.github.io',  // Replace with your GitHub username
-  base: '/learn_programming' ,  // Match your repository name exactly
+  base: '/learn_programming',  // Match your repository name exactly
+  integrations: [icon()],
   vite: {
     plugins: [tailwindcss()],
   },
